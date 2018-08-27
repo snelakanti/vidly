4fb0cfc2e6c64809ae24c1108f6b039e2fdd7c87
snelakanti <vnelakanti@gmail.com>
2018-08-27 16:14:45 -0400

    create table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
-  state = {};
+  state = {
+    movies: getMovies()
+  };
   render() {
-    return <h1>Movies Component</h1>;
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map(movie => (
+            <tr>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   }
 }
 
