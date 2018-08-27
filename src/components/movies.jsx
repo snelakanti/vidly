d55edffc407b5ae286e8cae709686be4d7ee323e
snelakanti <vnelakanti@gmail.com>
2018-08-27 16:27:57 -0400

    update state (entry delete)

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,11 @@ class Movies extends Component {
     movies: getMovies()
   };
 
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
   render() {
     return (
       <table className="table">
@@ -20,14 +25,14 @@ class Movies extends Component {
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
               <td>
                 <button
-                  onClick={this.handleDelete}
+                  onClick={() => this.handleDelete(movie)}
                   className="btn btn-danger btn-sm"
                 >
                   Delete
